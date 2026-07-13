fea54b0e50643c1545c2373a824b32c1a2154af8
bharath2410 <bannu2410@gmail.com>
2026-07-13 13:51:32 +0400

    fix: static assets resolution paths for railway production deployment

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tracker', 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
